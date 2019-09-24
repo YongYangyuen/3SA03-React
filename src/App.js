cb49bf82a26d7e84acc629fa1c8be76640e3d583
YongYangyuen <kkingza@hotmail.com>
2019-09-24 11:40:47 +0700

    Start implementation

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import WordCard from './WordCard';
+
+const word = "Hello"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {
+          <WordCard value={word}/>
+        }
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/WordCard.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './App.css';
+import CharacterCard from './CharacterCard';
+import _ from 'lodash';
+
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: [],
+        completed: false
+    }
+}
+
+export default class WordCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = prepareStateFromWord(this.props.value)
+    }
+
+    activationHandler = (c) => {
+        let guess = [...this.state.guess, c]
+        this.setState({guess})
+        if(guess.length == this.state.chars.length) {
+            if(guess.join('').toString() == this.state.word) {
+                this.setState({guess: [], completed: true})
+            }
+            else {
+                this.setState({guess: [], attempt: this.state.attempt + 1})
+            }
+        }
+        console.log(`${c} has been activated.`)
+    }
+
+    render() {  
+        return(
+            <div className="App">
+                {
+                    Array.from(this.state.chars).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt} activationHandler={this.activationHandler}/>)
+                }
+            </div>
+        );
+    }
+}
